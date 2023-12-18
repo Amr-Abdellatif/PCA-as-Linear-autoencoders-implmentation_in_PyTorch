99dfb60e1be8a766b7001b9c7180bf2961a49e2d
Amr-Abdellatif <amrosama42@gmail.com>
2023-12-18 20:31:04 +0200

    updates

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # PCA-as-autoencoders-implmentation_in_PyTorch
+
 This Repo is about PCA implmentation PyTorch which acts as Autoencoder
+
+![Alt text](mathematical%20equation.JPG)
---
+++ utils.py
@@ -0,0 +1,113 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from torch.utils.data import Dataset, DataLoader
+
+from tqdm.autonotebook import tqdm
+
+
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+import pandas as pd
+
+import time
+
+class View(nn.Module):
+    def __init__(self, *shape):
+        super(View, self).__init__()
+        self.shape = shape
+    def forward(self, input):
+        return input.view(*self.shape)
+
+
+def train_network(model, loss_func, train_loader, val_loader=None, test_loader=None,score_funcs=None, 
+                         epochs=50, device="cpu", checkpoint_file=None, 
+                         lr_schedule=None, optimizer=None, disable_tqdm=False
+                        ):
+    """Train simple neural networks
+    
+    Keyword arguments:
+    model -- the PyTorch model / "Module" to train
+    loss_func -- the loss function that takes in batch in two arguments, the model outputs and the labels, and returns a score
+    train_loader -- PyTorch DataLoader object that returns tuples of (input, label) pairs. 
+    val_loader -- Optional PyTorch DataLoader to evaluate on after every epoch
+    test_loader -- Optional PyTorch DataLoader to evaluate on after every epoch
+    score_funcs -- A dictionary of scoring functions to use to evalue the performance of the model
+    epochs -- the number of training epochs to perform
+    device -- the compute lodation to perform training
+    lr_schedule -- the learning rate schedule used to alter \eta as the model trains. If this is not None than the user must also provide the optimizer to use. 
+    optimizer -- the method used to alter the gradients for learning. 
+    
+    """
+    if score_funcs == None:
+        score_funcs = {}#Empty set 
+    
+    to_track = ["epoch", "total time", "train loss"]
+    if val_loader is not None:
+        to_track.append("val loss")
+    if test_loader is not None:
+        to_track.append("test loss")
+    for eval_score in score_funcs:
+        to_track.append("train " + eval_score )
+        if val_loader is not None:
+            to_track.append("val " + eval_score )
+        if test_loader is not None:
+            to_track.append("test "+ eval_score )
+        
+    total_train_time = 0 #How long have we spent in the training loop? 
+    results = {}
+    #Initialize every item with an empty list
+    for item in to_track:
+        results[item] = []
+
+        
+    if optimizer == None:
+        #The AdamW optimizer is a good default optimizer
+        optimizer = torch.optim.AdamW(model.parameters())
+        del_opt = True
+    else:
+        del_opt = False
+
+    #Place the model on the correct compute resource (CPU or GPU)
+    model.to(device)
+    for epoch in tqdm(range(epochs), desc="Epoch", disable=disable_tqdm):
+        model = model.train()#Put our model in training mode
+
+        total_train_time += run_epoch(model, optimizer, train_loader, loss_func, device, results, score_funcs, prefix="train", desc="Training")
+        
+        results["epoch"].append( epoch )
+        results["total time"].append( total_train_time )
+        
+      
+        if val_loader is not None:
+            model = model.eval() #Set the model to "evaluation" mode, b/c we don't want to make any updates!
+            with torch.no_grad():
+                run_epoch(model, optimizer, val_loader, loss_func, device, results, score_funcs, prefix="val", desc="Validating")
+                
+        #In PyTorch, the convention is to update the learning rate after every epoch
+        if lr_schedule is not None:
+            if isinstance(lr_schedule, torch.optim.lr_scheduler.ReduceLROnPlateau):
+                lr_schedule.step(results["val loss"][-1])
+            else:
+                lr_schedule.step()
+                
+        if test_loader is not None:
+            model = model.eval() #Set the model to "evaluation" mode, b/c we don't want to make any updates!
+            with torch.no_grad():
+                run_epoch(model, optimizer, test_loader, loss_func, device, results, score_funcs, prefix="test", desc="Testing")
+        
+        
+        if checkpoint_file is not None:
+            torch.save({
+                'epoch': epoch,
+                'model_state_dict': model.state_dict(),
+                'optimizer_state_dict': optimizer.state_dict(),
+                'results' : results
+                }, checkpoint_file)
+    if del_opt:
+        del optimizer
+
+    return pd.DataFrame.from_dict(results)
